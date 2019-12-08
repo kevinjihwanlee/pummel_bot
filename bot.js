bcb15a72be3be01c0bc89fc58fde2dbc3c94a69e
Kevin J Lee <kevin.jihwan.lee@gmail.com>
2019-12-07 23:34:09 -0600

    Integrate Twitch API, can manually check channel stream status

--- bot.js
+++ bot.js
@@ -1,5 +1,15 @@
 const Discord = require('discord.js');
+const axios = require('axios');
 const auth = require('./auth.json');
+
+const instance = axios.create({
+  baseURL: 'https://api.twitch.tv/kraken/',
+  timeout: 1000,
+  headers: {
+    'Accept': 'application/vnd.twitchtv.v5+json',
+    'Client-ID': auth.twitch_client_id
+  }
+});
 
 // Initialize Discord Bot
 const client = new Discord.Client()
@@ -8,10 +18,32 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// TODO switch to ES6 so we can use async/await
+
+// TODO function for translating username to user ID
+// TODO make this function accept the user ID
+const isKingOnline = (msg) => {
+  instance.get('streams/29093531')
+    .then(response => {
+      if (response.stream !== null) {
+        msg.reply("Alvin is online. https://twitch.tv/songmeister")
+      } else {
+        msg.reply("Alvin is not online :(")
+      }
+      return response
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('pong');
   }
+  if (msg.content === '!alvin') {
+    streamStatus = isKingOnline(msg)
+  }
 });
 
-client.login(auth.token)
+client.login(auth.discord_token)
